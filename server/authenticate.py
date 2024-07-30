96d4e589c02bbca6218e8921909cd6e783cff0fa
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-07-30 12:35:20 +0300

    Created User routes

--- server/authenticate.py
+++ server/authenticate.py
@@ -1,6 +1,7 @@
 from flask import  request, make_response, jsonify, session, Blueprint
-from flask_restful import Resource, Api
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_restful import Resource, Api, reqparse
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required, get_jwt_identity, current_user
+from functools import wraps
 
 # Local imports
 from config import db, app
@@ -8,9 +9,22 @@ from config import db, app
 from models import User
 
 authenticate_bp = Blueprint('authenticate_bp',__name__, url_prefix='/user')
+auth_api = Api(authenticate_bp)
 
 jwt = JWTManager()
 auth_api = Api(authenticate_bp)
+
+def init_jwt(app):
+    jwt.init_app(app) 
+
+@jwt.user_identity_loader
+def user_identity_lookup(user_id):
+    return user_id
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return User.query.filter_by(id=identity).one_or_none()
 
 def allow(*allowed_roles):
     def wrapper(fn):
@@ -27,3 +41,65 @@ def allow(*allowed_roles):
         return decorator
 
     return wrapper
+
+register_args = reqparse.RequestParser()
+register_args.add_argument('email')
+register_args.add_argument('password')
+register_args.add_argument('username')
+
+
+login_args = reqparse.RequestParser()
+login_args.add_argument('email')
+login_args.add_argument('password')
+
+
+class Register(Resource):
+
+    def post(self):
+
+        data = register_args.parse_args()
+        email = data.get('email')
+        username = data.get('username')
+        password = data.get('paddword')
+        if username and email and password:
+            if User.query.filter_by(username=username) or User.query.fliter_by(email=email):
+                return {'error': 'User already exists'}
+            new_user = User(username=username, email=email)
+            new_user.set_password(password)
+            db.session.add(new_user)
+            db.session.commit()
+
+            session['user_id'] = new_user.id
+            access_token = create_access_token(identity = new_user.id)
+
+            return {'User': new_user.to_dict(), 'access_token': access_token}
+
+
+        return {'error': '422 Unprocessable Entity'}, 422
+
+
+class Login(Resource):
+
+    def post(self):
+        data = login_args.parse_args()
+        password = data.get('password')
+        # check if the user exists in our db
+        user = User.query.filter_by(email=data.get('email')).first()
+        if user and user.authenticate(password):
+            session['user_id'] =user.id
+            # login
+            token = create_access_token(identity=user.id)
+            refresh_token = create_refresh_token(identity=user.id)
+            return {"token": token, "refresh_token": refresh_token}
+
+
+    @jwt_required(refresh=True)
+    def get(self):
+        token = create_access_token(identity= current_user.id )
+        return {"token":token}
+
+
+
+# routes
+auth_api.add_resource(Register, '/register')
+auth_api.add_resource(Login, '/login')
--- server/models.py
+++ server/models.py
@@ -29,19 +29,12 @@ class User(db.Model, SerializerMixin):
             raise ValueError(f'User must have a {key}')
         return value
 
-    @hybrid_property
-    def password_hash(self):
-        return self._password_hash
-    
-    @password_hash.setter
-    def password_hash(self, password):
-        password_hash = bcrypt.generate_password_hash(
-            password.encode('utf-8'))
-        self._password_hash = password_hash.decode('utf-8')
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf8')
     
     def authenticate(self, password):
         return bcrypt.check_password_hash(
-            self._password_hash, password.encode('utf-8'))
+            self._password_hash, password)
 
 class Product(db.Model, SerializerMixin):
     __tablename__ = 'products'
