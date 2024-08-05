1d0aa4def84b982a70d76830e57ef5f61db63349
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-08-05 14:04:14 +0300

    added som config data

--- server/app.py
+++ server/app.py
@@ -5,6 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flask_restful import  Api
+from models import User
 
 
 
@@ -19,12 +20,12 @@ def create_app(config_name=None):
     jwt.init_app(app)
     api.init_app(app)
     cors.init_app(app)
-    
+
     from authenticate import authenticate_bp
     from products import product_bp
     from orders import order_bp
     from wishlist import wishlist_bp
-
+    
     app.register_blueprint(authenticate_bp)
     app.register_blueprint(product_bp)
     app.register_blueprint(order_bp)
@@ -38,5 +39,5 @@ def create_app(config_name=None):
 
 
 if __name__ == '__main__':
-    app = create_app()
+    app = create_app('development')
     app.run(port=5555, debug=True)
