727be3872e0a3b999dff6eba41df9591cfc2cea3
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-08-08 17:48:03 +0300

    fixed db init

--- server/test_products.py
+++ server/test_products.py
@@ -28,6 +28,12 @@ class ProductsTestCase(unittest.TestCase):
             identity = jwt_data["sub"]
             return User.query.filter_by(id=identity).one_or_none()
 
+        # Define and register the user lookup callback
+        @self.jwt.user_lookup_loader
+        def user_lookup_callback(_jwt_header, jwt_data):
+            identity = jwt_data["sub"]
+            return User.query.filter_by(id=identity).one_or_none()
+
     def tearDown(self):
         # Clean up the database
         db.session.remove()
